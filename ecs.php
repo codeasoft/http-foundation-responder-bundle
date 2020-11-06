1ccb6086c118a9a1ba5c09fcf7a9ae50af3efb59
Edo Barnáš <eduard.barnas@codea.eu>
2020-11-06 21:22:18 +0100

    Configure the test environment

---
+++ ecs.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use SlevomatCodingStandard\Sniffs\TypeHints\PropertyTypeHintSniff;
+use SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void
+{
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $parameters->set(Option::SETS, [
+        SetList::COMMON,
+        SetList::CLEAN_CODE,
+        SetList::DEAD_CODE,
+        SetList::PHP_71,
+        SetList::PSR_12,
+        SetList::SYMFONY,
+    ]);
+
+    $parameters->set(Option::SKIP, [
+        PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer::class => null,
+        PropertyTypeHintSniff::class . 'MissingTraversableTypeHintSpecification' => null,
+        ReturnTypeHintSniff::class . 'MissingTraversableTypeHintSpecification' => null,
+        PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer::class => [
+            __DIR__ . '/src/DependencyInjection/Configuration.php'
+        ],
+        PhpCsFixer\Fixer\PhpUnit\PhpUnitStrictFixer::class => [
+            __DIR__ . '/tests/ResponderListenerTest.php'
+        ],
+    ]);
+};
