442065ade445c6ad8a3a655f5bec6119690e7905
Edo Barnáš <iam@edobarnas.com>
2021-02-16 11:51:32 +0100

    Rework DI compilation

---
+++ src/DependencyInjection/Compiler/RegisterFlexResponderPass.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Bundle\Responder\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+use Tuzex\Responder\FlexResponder;
+use Tuzex\Responder\Middleware\CreateResponseMiddleware;
+
+final class RegisterFlexResponderPass implements CompilerPassInterface
+{
+    private function setup(Definition $responder, array $middlewareIds): Definition
+    {
+        return $responder->addMethodCall('extend', array_map(
+            fn (string $middlewareId): Reference => new Reference($middlewareId), $middlewareIds)
+        );
+    }
+
+    public function process(ContainerBuilder $container): void
+    {
+        $responderId = FlexResponder::class;
+        $middlewareIds = array_keys(
+            $container->findTaggedServiceIds('tuzex.responder.middleware')
+        );
+
+        $container->setDefinition($responderId, $this->define($responderId, $middlewareIds));
+    }
+
+    private function define(string $responderId, array $middlewareIds): Definition
+    {
+        $responder = new Definition($responderId, [
+            new Reference(CreateResponseMiddleware::class),
+        ]);
+
+        return $this->setup($responder, $middlewareIds);
+    }
+}
---
+++ src/DependencyInjection/Compiler/RegisterResponseMiddlewarePass.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Bundle\Responder\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+use Tuzex\Responder\Middleware\CreateResponseMiddleware;
+
+final class RegisterResponseMiddlewarePass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        $middlewareId = CreateResponseMiddleware::class;
+        $factoryIds = array_keys(
+            $container->findTaggedServiceIds('tuzex.responder.response_factory')
+        );
+
+        $container->setDefinition($middlewareId, $this->define($middlewareId, $factoryIds));
+    }
+
+    private function define(string $middlewareId, array $factoryIds): Definition
+    {
+        return new Definition($middlewareId, array_map(
+            fn (string $factoryId): Reference => new Reference($factoryId), $factoryIds)
+        );
+    }
+}
