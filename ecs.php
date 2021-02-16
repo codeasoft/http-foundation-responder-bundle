6865449f693bb0a829c7d9453c5597b5a7b85016
Edo Barnáš <iam@edobarnas.com>
2021-02-16 11:56:06 +0100

    Update ECS configuration

--- ecs.php
+++ ecs.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Fixer\Basic\BracesFixer;
+use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
+use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\CodingStandard\Fixer\Spacing\StandaloneLinePromotedPropertyFixer;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
@@ -17,21 +21,16 @@ return static function (ContainerConfigurator $containerConfigurator): void
     $parameters->set(Option::SETS, [
         SetList::COMMON,
         SetList::CLEAN_CODE,
-        SetList::DEAD_CODE,
-        SetList::PHP_71,
         SetList::PSR_12,
         SetList::SYMFONY,
     ]);
 
     $parameters->set(Option::SKIP, [
-        PhpCsFixer\Fixer\Basic\BracesFixer::class => null,
-        PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer::class => null,
-        PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer::class => [
+        BracesFixer::class => null,
+        NotOperatorWithSuccessorSpaceFixer::class => null,
+        MethodChainingIndentationFixer::class => [
             __DIR__ . '/src/DependencyInjection/Configuration.php',
         ],
-        Symplify\CodingStandard\Fixer\Spacing\StandaloneLinePromotedPropertyFixer::class => null,
-        SlevomatCodingStandard\Sniffs\TypeHints\PropertyTypeHintSniff::class . 'MissingTraversableTypeHintSpecification' => null,
-        SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff::class . 'MissingTraversableTypeHintSpecification' => null,
-        SlevomatCodingStandard\Sniffs\Classes\DisallowMultiPropertyDefinitionSniff::class . '.DisallowedMultiPropertyDefinition' => null,
+        StandaloneLinePromotedPropertyFixer::class => null,
     ]);
 };
