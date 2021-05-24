d15d6d99796cfa1126b4696797c3946d968d60ce
Edo <iam@edobarnas.com>
2021-05-24 20:35:10 +0200

    Reconfigure ECS

--- ecs.php
+++ ecs.php
@@ -2,35 +2,25 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\Basic\BracesFixer;
-use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
-use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\CodingStandard\Fixer\Spacing\StandaloneLinePromotedPropertyFixer;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void
-{
+return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
 
-    $parameters->set(Option::SETS, [
-        SetList::COMMON,
-        SetList::CLEAN_CODE,
-        SetList::PSR_12,
-        SetList::SYMFONY,
+    $parameters->set(Option::SKIP, [
+        PhpCsFixer\Fixer\Basic\BracesFixer::class => null,
     ]);
 
-    $parameters->set(Option::SKIP, [
-        BracesFixer::class => null,
-        NotOperatorWithSuccessorSpaceFixer::class => null,
-        MethodChainingIndentationFixer::class => [
-            __DIR__ . '/src/DependencyInjection/Configuration.php',
-        ],
-        StandaloneLinePromotedPropertyFixer::class => null,
-    ]);
+    $containerConfigurator->import(SetList::COMMON);
+    $containerConfigurator->import(SetList::CLEAN_CODE);
+    $containerConfigurator->import(SetList::PSR_12);
+    $containerConfigurator->import(SetList::SYMFONY);
 };
+
+
